52b529385f582958c84c305b891e6566ae191c6c
L HT <LHT@807663418@qq.com>
2018-12-30 12:30:16 +0800

    多线程测试

--- controlcenter/src/main/java/com/iot/controlcenter/Main.java
+++ controlcenter/src/main/java/com/iot/controlcenter/Main.java
@@ -1,17 +1,40 @@
 package com.iot.controlcenter;
 
-import com.iot.controlcenter.customerGroup.CustomerGroup;
-import com.iot.controlcenter.producergroup.ProducerHandle;
+import org.apache.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
+      new Test().start();
+      new Test().start();
+      new Test().start();
+      new Test().start();
+    }
+}
 
+class Test extends Thread{
+    private static ConcurrentHashMap<String,Integer> concurrentHashMap=new ConcurrentHashMap<>();
+    private Logger logger=Logger.getLogger(Test.class);
+    @Override
+    public void run() {
+       synchronized (Test.class){
+           for (int i = 0; i < 5; i++) {
+               logger.info(Thread.currentThread().getName()+"正在添加数据"+i);
+               concurrentHashMap.put(Thread.currentThread().getName(),i);
+           }
+       }
+    }
+}
+
+ class insertData{
+    private Logger logger=Logger.getLogger(insertData.class);
+    private ArrayList<String> arrayList=new ArrayList<>();
+    public synchronized void add(){
+        for (int i = 0; i < 5; i++) {
+            logger.info(Thread.currentThread().getName()+"正在插入数据"+i);
+            arrayList.add(String.valueOf(i));
+        }
     }
 }
