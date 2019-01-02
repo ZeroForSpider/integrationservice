1ee8f6b58303bd6be14d3c858a6b4977830fe188
L HT <LHT@807663418@qq.com>
2019-01-02 09:34:27 +0800

    取消和生产者的一些配置信息

--- controlcenter/src/main/java/com/iot/controlcenter/producergroup/ProducerHandle.java
+++ controlcenter/src/main/java/com/iot/controlcenter/producergroup/ProducerHandle.java
@@ -62,15 +62,6 @@ public class ProducerHandle implements Runnable {
             properties.put(entry.getKey(), entry.getValue());
         }
         logger.info("properties  " + properties);
-        //   Properties properties = new Properties();
-//        properties.put("bootstrap.servers", "119.29.193.187:9092");
-//        properties.put("acks", "all");
-//        properties.put("retries", 0);
-//        properties.put("batch.size", 16384);
-//        properties.put("linger.ms", 1);
-//        properties.put("buffer.memory", 33554432);
-//        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         return properties;
     }
 
